845eade959f974f54582ca3c7700bfc243e4f144
Nicole Dow <nikki.dow71@gmail.com>
2019-04-19 23:46:07 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,10 +5,9 @@ def line(katz_deli)
   
   if katz_deli.size == 0
     puts "The line is currently empty."
-  else
-    katz_deli.each_with_index do |person, i|
-      phrase += "#{i + 1}. #{person} "
-    end
-    puts phrase
-  end
+
+   katz_deli.each_with_index do |person, i|
+     phrase += "#{i + 1}. #{person} "
+   end
+  puts phrase
 end
