d04dca05ad38261282a8452173cdd5fe819e7d0a
Nicole Dow <nikki.dow71@gmail.com>
2019-04-19 23:38:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,19 +1,9 @@
 katz_deli = []
 
-def line(katz_deli)
-  line_phrase = "The line is currently: "
+def line
   if katz_deli.size == 0
     puts "The line is currently empty."
   else
-    katz_deli.each_with_index do |person, idx|
-      line_phrase += "#{idx + 1}. #{person} "
-      puts line_phrase
-    end
-end
-
-
-def take_a_number(katz_deli, person)
-  place = katz_deli.index(person)
-  
-  puts "Welcome, #{person}. You are number #{place} in line."
+    
+  end
 end
