3a9cc86c493786492a49b5543331b1a1718a7190
Nicole Dow <nikki.dow71@gmail.com>
2019-04-20 00:07:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,7 +22,7 @@ end
 
 def now_serving(katz_deli)
   if katz_deli.size == 0
-    puts "Ther is nobody waiting to be served!"
+    puts "There is nobody waiting to be served!"
   else
     current_customer = katz_deli.shift
     puts "Currently serving #{current_customer}."
